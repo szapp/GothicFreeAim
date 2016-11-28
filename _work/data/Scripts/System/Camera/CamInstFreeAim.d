6146082001d05e32c94685fdc9ca6ad83b73330d
szapp <dev.szapp@gmail.com>
2016-11-28 21:15:56 +0100

    Change license to MIT and bump version to v0.1.1

--- _work/data/Scripts/System/Camera/CamInstFreeAim.d
+++ _work/data/Scripts/System/Camera/CamInstFreeAim.d
@@ -1,26 +1,21 @@
 /*
  * Free aim camera mode
  *
- * G2 Free Aim v0.1.0 - Free aiming for the video game Gothic 2 by Piranha Bytes
+ * G2 Free Aim v0.1.1 - Free aiming for the video game Gothic 2 by Piranha Bytes
  * Copyright (C) 2016  mud-freak (@szapp)
  *
  * This file is part of G2 Free Aim.
  * <http://github.com/szapp/g2freeAim>
  *
  * G2 Free Aim is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the MIT License.
  * On redistribution this notice must remain intact and all copies must
  * identify the original author.
  *
  * G2 Free Aim is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with G2 Free Aim.  If not, see <http://www.gnu.org/licenses/>.
+ * MIT License for more details (see the file LICENSE).
  */
 
 INSTANCE CamModFreeAim (CCamSys_Def)
