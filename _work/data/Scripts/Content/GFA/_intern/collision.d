687fbdce2428869d606fc84f1f62e704e9702d68
szapp <dev.szapp@gmail.com>
2017-09-23 22:09:34 +0200

    Bump version to v1.0.0-beta.17

--- _work/data/Scripts/Content/GFA/_intern/collision.d
+++ _work/data/Scripts/Content/GFA/_intern/collision.d
@@ -1,7 +1,7 @@
 /*
  * Custom projectile collision behaviors feature
  *
- * Gothic Free Aim (GFA) v1.0.0-beta.16 - Free aiming for the video games Gothic 1 and Gothic 2 by Piranha Bytes
+ * Gothic Free Aim (GFA) v1.0.0-beta.17 - Free aiming for the video games Gothic 1 and Gothic 2 by Piranha Bytes
  * Copyright (C) 2016-2017  mud-freak (@szapp)
  *
  * This file is part of Gothic Free Aim.
